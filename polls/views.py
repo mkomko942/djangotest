c36220eea1223f821e9714cc52e3823cef7ace05
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-08-30 06:08:43 +0000

    add bootstrap template

---
+++ polls/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Question
+
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
